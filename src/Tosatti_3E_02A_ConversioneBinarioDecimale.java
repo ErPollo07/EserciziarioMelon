e3a67e553a767843892aa82cfcdcc271642e3466
ErPolloSgravato <nicola.tosatti@iisviolamarchesini.edu.it>
2023-12-05 15:43:09 +0100

    Aggiunti commenti

--- src/Tosatti_3E_02A_ConversioneBinarioDecimale.java
+++ src/Tosatti_3E_02A_ConversioneBinarioDecimale.java
@@ -8,6 +8,7 @@ public class Tosatti_3E_02A_ConversioneBinarioDecimale {
         String numeroBinario = ""; // input
         double numeroDecimale = 0; // output
 
+        // variabili di calcolo
         double carattere = 0;
         double pow = 0;
         boolean flagBinario = true;
@@ -18,28 +19,32 @@ public class Tosatti_3E_02A_ConversioneBinarioDecimale {
         System.out.println("Inserisci un numero binario: ");
         numeroBinario = tastiera.next();
 
-
+        // sto dentro al ciclo fino a quando non arrivo alla fine del numero
         for (int i = numeroBinario.length(); i > 0; i--) {
 
+            // Prendo il carattere
             carattere  = Character.getNumericValue(numeroBinario.charAt(i - 1));
+
+            // Controllo se il carattere é 0 o 1
+            // Se si allora calcolo il numero
             if (carattere == 1 || carattere == 0) {
-                // nuumeroDecimale = numeroDecimale (int)char * 10**i
+                // FORMULA: numeroDecimale = numeroDecimale (int)char * 10**pow
                 numeroDecimale = numeroDecimale + carattere * Math.pow(2.0, pow);
             }
+
+            // se il carattere non é valido
             else {
                 System.out.println("Numero non valido");
                 flagBinario= false;
                 break;
             }
 
-
             // Aggiorno l'esponente
             pow++;
         }
 
+        // se il numero é valido allora stampo il risultato
         if (flagBinario)
             System.out.println(numeroDecimale);
-
-
     }
 }
