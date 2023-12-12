195c671c173ac7877a598b28b1d9ca8a82697ec6
ErPolloSgravato <nicola.tosatti@iisviolamarchesini.edu.it>
2023-12-12 15:56:52 +0100

    For loop, print ascii art Add for loop for cooking the egg Add sout in every Egg method Add pause character before start cooking

--- src/Tosatti_3E_05A_CotturaUovo.java
+++ src/Tosatti_3E_05A_CotturaUovo.java
@@ -5,41 +5,77 @@ temporizzare la cottura dell'uovo simulandone le fasi di cottura con delle figur
 utilizzando dei caratteri ascii.
 */
 
+import java.util.Scanner;
+
 public class Tosatti_3E_05A_CotturaUovo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String _pause; // Character for pause
+
+        System.out.println("Benvenuti al ristorante");
+
+        System.out.println();
+        _pause = scanner.nextLine();
+
+        // mettere tempo 3m
+        // fase di test tempo 20s
+        for (int i = 20; i > 0; i--) {
+
+            UovoCheBolle();
+
+            System.out.println("\n\nTimer: " + i);
+            Wait(1000);
+            ClrScr();
+        }
 
 
     }
 
     private static void UovoNonBollito() {
-        // stampa l'immagine dell'uovo
-        /*
-          ,'"`.
-         /     \
-        :       :
-        :       :
-         `.___,'
-          */
+
+        System.out.println("""
+                  ,'"`.
+                 /     \\
+                :       :
+                :       :
+                 `.___,'
+                """);
     }
 
     private static void UovoCheBolle() {
+
+        System.out.println("""
+                  °  ,'"`. °   °
+                °   /     \\ °
+               °   :       :  °
+                 ° :       : °
+              °   ° `.___,' °   °
+                """);
+
         /*
-           °  ,'"`. °   °
-         °   /     \ °
-        °   :       :  °
-          ° :       : °
-       °   ° `.___,' °   °
+
          */
     }
 
-    private static void uovoCotto() {
-        /*
-              ,'"`.
-             /  _  \
-            : /   \ :
-            : \___/ :
-             `.___,'
-        */
+    private static void UovoCotto() {
+
+        System.out.println("""
+                    ,'"`.
+                   /  _  \\
+                  : /   \\ :
+                  : \\___/ :
+                   `.___,' 
+                """);
+    }
+
+    private static void Wait(int milliSecond) {
+        try {
+            Thread.sleep(milliSecond);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void ClrScr() {
