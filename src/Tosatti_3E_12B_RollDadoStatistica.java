3b402da7f6bb3a38461bbd316c28a58ce28d3a34
ErPolloSgravato <nicola.tosatti@iisviolamarchesini.edu.it>
2024-01-10 09:34:47 +0100

    Done Es 12B frequenze

--- src/Tosatti_3E_12B_RollDadoStatistica.java
+++ src/Tosatti_3E_12B_RollDadoStatistica.java
@@ -1,13 +1,53 @@
 import java.util.Random;
 
 public class Tosatti_3E_12B_RollDadoStatistica {
+    public static void main(String[] args) {
 
-    private static int valoreRandom(int maxValue, int minValue) {
+        int val;
+
+        int[] frequenze = new int[6];
+
+        for (int i = 0; i < 100; i++) {
+
+            val = valoreRandom(1,7);
+            // System.out.printf("Valore %d\n", val);
+            // %f float
+            // %s string
+
+            FacceDado(val);
+
+            switch (val) {
+                case 1:
+                    frequenze[0]++;
+                    break;
+                case 2:
+                    frequenze[1]++;
+                    break;
+                case 3:
+                    frequenze[2]++;
+                    break;
+                case 4:
+                    frequenze[3]++;
+                    break;
+                case 5:
+                    frequenze[4]++;
+                    break;
+                case 6:
+                    frequenze[5]++;
+                    break;
+            }
+        }
+
+        for (int i = 0; i < frequenze.length; i++) {
+
+            System.out.printf("Faccia%d: %d\n", i + 1,  frequenze[i]);
+        }
+    }
+
+    private static int valoreRandom(int minValue, int maxValue) {
         Random random = new Random();
 
-        int d = 0;
-
-        return d;
+        return random.nextInt(minValue, maxValue);
     }
 
 
